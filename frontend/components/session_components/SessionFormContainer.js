c3bd2ce5c552efe75a257292bb0eda5437454ebf
vilyapilya <poemsreader@gmail.com>
2017-06-28 14:42:58 -0700

    fixes the signe up link

--- frontend/components/session_components/SessionFormContainer.js
+++ frontend/components/session_components/SessionFormContainer.js
@@ -13,8 +13,7 @@ const mapStateToProps = ({ session }) => {
 };
 
 const mapDispatchToProps = (dispatch, { location }) => {
-  // const formType = location.pathname.slice(1);
-  const formType = 'login';
+  const formType = location.pathname.slice(1);
   const processForm = (formType === 'login') ? login : signup;
   return {
     processForm: user => dispatch(processForm(user)),
